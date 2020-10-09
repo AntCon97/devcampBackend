17999eaacc85bdecce20bb802f8c102321a469e6
Anthony <antcon1000@gmail.com>
2020-10-09 12:07:49 -0400

    added data, connected to mongoDB and compass, added functionalality to add bootcamp to database

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db')
+
 
 // route files
 const bootcamps = require('./routes/bootcamps');
+const { connect } = require('mongoose');
 
 //Loade env vars
 dotenv.config({
   path: './config/config.env',
 });
 
+//connect to database
+connectDB();
+
+
 const app = express();
+
+//Body Parser
+app.use(express.json())
 
 // dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -22,7 +33,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold)
 );
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err,promis) => {
+  console.log(`Error: ${err.message}`.red.bold);
+  //close sever and exit process
+  server.close(() => process.exit(1))
+})
